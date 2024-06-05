ccfd3bdeaddd645f99725d4bddc0741f7475c7ec
mhmd <ms.mhmdsalah@gmail.com>
2024-06-05 15:41:57 +0300

    scroll animation

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,16 @@
 "use strict";
+let boxs = document.querySelectorAll(".box");
+window.addEventListener("scroll", checkBoxs);
+checkBoxs();
+function checkBoxs() {
+    const triggerBottom = window.innerHeight / 5 * 4;
+    boxs.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) {
+            box.classList.add("show");
+        }
+        else {
+            box.classList.remove("show");
+        }
+    });
+}
